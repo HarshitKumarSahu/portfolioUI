3eb7dce60e01a6a4c551a1cda3c9d20b86622eb5
HarshitKumarSahu <poltuharshit14@gmail.com>
2025-08-10 20:30:33 +0530

    added gsap

--- src/main.js
+++ src/main.js
@@ -1,1 +1,49 @@
 import './testing.css'
+
+import gsap from 'gsap'
+
+let svgs = document.querySelectorAll("svg");
+
+svgs.forEach(svg => {
+    svg.addEventListener("mouseenter", () => {
+        gsap.to(svg, {
+            fill: "orange"
+        });
+    })
+    svg.addEventListener("mouseleave", () => {
+        gsap.to(svg, {
+            fill: "#363636"
+        });
+    })
+});
+
+let as = document.querySelectorAll("a");
+as.forEach(a => {
+    a.addEventListener("mouseenter", () => {
+        gsap.to(a, {
+            color: "orange"
+        });
+    })
+    a.addEventListener("mouseleave", () => {
+        gsap.to(a, {
+            color: "#363636"
+        });
+    })
+});
+
+let btns = document.querySelectorAll("button");
+btns.forEach(btn => {
+    btn.addEventListener("mouseenter", () => {
+        gsap.to(btn, {
+            color: "orange",
+            // duration: 1
+            borderColor: "orange"
+        });
+    })
+    btn.addEventListener("mouseleave", () => {
+        gsap.to(btn, {
+            color: "#363636",
+            borderColor: "#363636"
+        });
+    })
+});
